db1856fdb1b70b269fc3cf1ee623ed53ee3843aa
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-10-18 03:29:20 +0200

    Hello world

--- Hello_test.py
+++ Hello_test.py
@@ -5,12 +5,23 @@ from tkinter import *
 root = Tk()
 
 # Creating a label for the root widget
-my_label = Label(root, text="Hello World!")
+''' As python is object oriented we can 
+write the code as follow OR
+like the commented few lines in the second paragraph
+But the downside for this approach is the code readability 
+as with more complicated GUI you will need many methods
+'''
+my_label1 = Label(root, text="Hello World!").grid(row=0, column=0)
+my_label2 = Label(root, text="I am Kareem...").grid(row=1, column=5)
+my_label3 = Label(root, text="                   ").grid(row=1, column=1)
 # packing the label to the screen of the app
-my_label.pack()
+#my_label1.grid(row=0, column=0)  # top left corner
+#my_label2.grid(row=1, column=5)  # the text to the right
+#my_label3.grid(row=1, column=1)  # created an empty column in the middle with this many space string
+
 
 # Creating the event loop on the root widget
 
 root.mainloop()
-b
+
 
---
+++ creating buttons.py
@@ -0,0 +1,10 @@
+# Importing Tkinter
+from tkinter import *
+
+# calling the root function and assigin it to tk()
+root = Tk()
+
+
+
+
+root.mainloop()
