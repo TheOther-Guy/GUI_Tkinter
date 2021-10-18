f05afbfde5dc00cd650675f2ac6a16cab3131f37
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-10-18 02:10:53 +0200

    .

---
+++ Hello_test.py
@@ -0,0 +1,16 @@
+# Importing Tkinter
+from tkinter import *
+
+# calling the root function and assigin it to tk()
+root = Tk()
+
+# Creating a label for the root widget
+my_label = Label(root, text="Hello World!")
+# packing the label to the screen of the app
+my_label.pack()
+
+# Creating the event loop on the root widget
+
+root.mainloop()
+
+
