64a2e5afc692b1fdaece9f5de11c06e0da7a5524
kareem_722@hotmail.com <49558122+TheOther-Guy@users.noreply.github.com>
2021-10-18 03:35:40 +0200

    disabled button with state=disabled

--- creating buttons.py
+++ creating buttons.py
@@ -4,6 +4,13 @@ from tkinter import *
 # calling the root function and assigin it to tk()
 root = Tk()
 
+# adding the button 
+# and notice disabled here make the button unclickable
+mybutton = Button(root, text='click me!', state="disabled") 
+# packing the Button to the screen 
+mybutton.pack()
+
+
 
 
 
